d873f9e747a0c4bfeee6839c5bb1a418ba4e38e4
Kyujin Cho <thy2134@me.com>
2016-01-30 01:46:05 +0900

    resolved privacy problem.

--- src/DCGreener.java
+++ src/DCGreener.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 public class DCGreener {
 
     public static void main(String[] args) {
-        String id = "jh0621";
-        String pw = "qetu0810";
+        String id = "yousuck"; // 본인의 iD
+        String pw = "foobar"; // 본인의 pW
 
         ConnectDC login = new ConnectDC(id); // 갤로그에서 댓 달린 글 고유번호 / 갤 ID 불러오기.
 
