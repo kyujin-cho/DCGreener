a653698a76c962589e149d606d85ffef9af5bd53
Kyujin Cho <thy2134@me.com>
2016-01-30 01:54:40 +0900

    getting user id/pw by keyboard input.

--- src/DCGreener.java
+++ src/DCGreener.java
@@ -3,13 +3,17 @@
  */
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class DCGreener {
 
     public static void main(String[] args) {
-        String id = "yousuck"; // 본인의 iD
-        String pw = "foobar"; // 본인의 pW
+        Scanner s = new Scanner(System.in);
+        System.out.println("ID? ");
+        String id = s.nextLine(); // 키보드로 입력받는 본인의 ID. 키보드로 입력받지 않고 싶으면 s.nextLine() 부분을 "자신의 ID" 로 변경.
+        System.out.println("PW? ");
+        String pw = s.nextLine(); // 키보드로 입력받는 본인의 PW. 키보드로 입력받지 않고 싶으면 s.nextLine() 부분을 "자신의 암호" 로 변경.
 
         ConnectDC login = new ConnectDC(id); // 갤로그에서 댓 달린 글 고유번호 / 갤 ID 불러오기.
 
